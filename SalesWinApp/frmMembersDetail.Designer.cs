3f4f7cb9d8a317121d61c6e6a517b07058a8e9f8
Mai Hữu Tài <huutaia21010@gmail.com>
2022-07-10 14:25:54 +0700

    Fix unsaved frmMembersDetail

--- SalesWinApp/frmMembersDetail.Designer.cs
+++ SalesWinApp/frmMembersDetail.Designer.cs
@@ -28,12 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMemberId = new System.Windows.Forms.TextBox();
+            this.txEmail = new System.Windows.Forms.TextBox();
+            this.txtRoleName = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbMemberId = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbRoleName = new System.Windows.Forms.Label();
+            this.lbCompanyName = new System.Windows.Forms.Label();
+            this.lbCity = new System.Windows.Forms.Label();
+            this.lbCountry = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMemberId
+            // 
+            this.txtMemberId.Location = new System.Drawing.Point(166, 52);
+            this.txtMemberId.Name = "txtMemberId";
+            this.txtMemberId.Size = new System.Drawing.Size(260, 27);
+            this.txtMemberId.TabIndex = 0;
+            // 
+            // txEmail
+            // 
+            this.txEmail.Location = new System.Drawing.Point(166, 133);
+            this.txEmail.Name = "txEmail";
+            this.txEmail.Size = new System.Drawing.Size(260, 27);
+            this.txEmail.TabIndex = 1;
+            // 
+            // txtRoleName
+            // 
+            this.txtRoleName.Location = new System.Drawing.Point(166, 228);
+            this.txtRoleName.Name = "txtRoleName";
+            this.txtRoleName.Size = new System.Drawing.Size(260, 27);
+            this.txtRoleName.TabIndex = 2;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(587, 60);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(249, 27);
+            this.txtCompanyName.TabIndex = 3;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(587, 129);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(249, 27);
+            this.txtCity.TabIndex = 4;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(960, 60);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(239, 27);
+            this.txtCountry.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(960, 126);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(239, 27);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // lbMemberId
+            // 
+            this.lbMemberId.AutoSize = true;
+            this.lbMemberId.Location = new System.Drawing.Point(62, 55);
+            this.lbMemberId.Name = "lbMemberId";
+            this.lbMemberId.Size = new System.Drawing.Size(82, 20);
+            this.lbMemberId.TabIndex = 7;
+            this.lbMemberId.Text = "Member Id";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(62, 140);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(46, 20);
+            this.lbEmail.TabIndex = 8;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbRoleName
+            // 
+            this.lbRoleName.AutoSize = true;
+            this.lbRoleName.Location = new System.Drawing.Point(62, 231);
+            this.lbRoleName.Name = "lbRoleName";
+            this.lbRoleName.Size = new System.Drawing.Size(83, 20);
+            this.lbRoleName.TabIndex = 9;
+            this.lbRoleName.Text = "Role Name";
+            // 
+            // lbCompanyName
+            // 
+            this.lbCompanyName.AutoSize = true;
+            this.lbCompanyName.Location = new System.Drawing.Point(465, 63);
+            this.lbCompanyName.Name = "lbCompanyName";
+            this.lbCompanyName.Size = new System.Drawing.Size(116, 20);
+            this.lbCompanyName.TabIndex = 10;
+            this.lbCompanyName.Text = "Company Name";
+            // 
+            // lbCity
+            // 
+            this.lbCity.AutoSize = true;
+            this.lbCity.Location = new System.Drawing.Point(465, 129);
+            this.lbCity.Name = "lbCity";
+            this.lbCity.Size = new System.Drawing.Size(34, 20);
+            this.lbCity.TabIndex = 11;
+            this.lbCity.Text = "City";
+            // 
+            // lbCountry
+            // 
+            this.lbCountry.AutoSize = true;
+            this.lbCountry.Location = new System.Drawing.Point(872, 67);
+            this.lbCountry.Name = "lbCountry";
+            this.lbCountry.Size = new System.Drawing.Size(60, 20);
+            this.lbCountry.TabIndex = 12;
+            this.lbCountry.Text = "Country";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(872, 126);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(70, 20);
+            this.lbPassword.TabIndex = 13;
+            this.lbPassword.Text = "Password";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(419, 358);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(671, 358);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMembersDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmMemberDetail";
+            this.ClientSize = new System.Drawing.Size(1222, 547);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbCountry);
+            this.Controls.Add(this.lbCity);
+            this.Controls.Add(this.lbCompanyName);
+            this.Controls.Add(this.lbRoleName);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbMemberId);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtRoleName);
+            this.Controls.Add(this.txEmail);
+            this.Controls.Add(this.txtMemberId);
+            this.Name = "frmMembersDetail";
+            this.Text = "frmMembersDetail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtMemberId;
+        private TextBox txEmail;
+        private TextBox txtRoleName;
+        private TextBox txtCompanyName;
+        private TextBox txtCity;
+        private TextBox txtCountry;
+        private TextBox txtPassword;
+        private Label lbMemberId;
+        private Label lbEmail;
+        private Label lbRoleName;
+        private Label lbCompanyName;
+        private Label lbCity;
+        private Label lbCountry;
+        private Label lbPassword;
+        private Button btnSave;
+        private Button btnClose;
     }
 }
